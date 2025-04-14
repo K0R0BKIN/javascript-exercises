1d24ba2a19fb650ce7809f80783e693f7d3c5a82
korobkin <nikita.korobkin.personal@gmail.com>
2025-04-15 00:00:20 +0300

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function(arr, arg) {
-    return arr.filter(value => value !== arg);
+const removeFromArray = function(arr, ...values) {
+    return arr.filter(item => {
+        let toRemove = false;
+        for (const value of values) {
+            if (item === value) {
+                toRemove = true;
+            }
+        }
+        return toRemove ? false : true;
+    });
 };
 
 // Do not edit below this line
