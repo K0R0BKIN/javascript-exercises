814484e562533ab81962cb540a371d3836245f90
korobkin <nikita.korobkin.personal@gmail.com>
2025-04-15 08:08:42 +0300

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    // Validate input
+    const args = [...arguments];
+    if (!args.every(arg => Number.isInteger(arg) && arg >= 0)) return "ERROR";
+    
+    // Calculate sum
+    let sum = 0;
+    for (let index = Math.min(a, b); index <= Math.max(a, b); index++) {
+        sum += index;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
