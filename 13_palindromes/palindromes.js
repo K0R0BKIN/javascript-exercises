a9ea406173de30e54a0cbc418e8a9596333d2ab9
korobkin <nikita.korobkin.personal@gmail.com>
2025-05-07 19:52:03 +0300

    Implement palindromes Compared the arrays by converting them into strings with join().

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const chars = str
+        .trim()
+        .split("")
+        .filter(c => c.match(/[A-za-z0-9]/))
+        .map(c => c.toLowerCase());
 
+    const charStr = chars.join("");
+    const reverseCharStr = chars.reverse().join("");
+    return charStr === reverseCharStr;
 };
+
+palindromes('Racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
