dbe736b0fe5cfc6dc49cb74db67325e6e8a32c7c
korobkin <nikita.korobkin.personal@gmail.com>
2025-05-07 20:48:06 +0300

    Implement fibonacci() Used while loop and at() method.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (position) {
+    if (position == 0) {
+        return 0;
+    } else if (position < 0) {
+        return "OOPS";
+    } else {
+        const seq = [1, 1];
+        while (seq.length < position) {
+            seq.push(seq.at(-2) + seq.at(-1));
+        }
+        return seq.at(-1);
+    }
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
